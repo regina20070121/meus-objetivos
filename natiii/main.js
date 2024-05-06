79e93a3b400fb25e7168da833d1aeeb7e329623c
regina20070121 <126187236+regina20070121@users.noreply.github.com>
2024-05-06 11:35:18 -0300

    ok

--- natiii/main.js
+++ natiii/main.js
@@ -16,9 +16,9 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-01-01T00:00:00");
-const tempoObjetivo2 = new Date("2024-06-05T00:00:00");
-const tempoObjetivo3 = new Date("2024-05-30T00:00:00");
-const tempoObjetivo4 = new Date("2025-05-01T00:00:00");
+const tempoObjetivo2 = new Date("2025-01-21T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-01T00:00:00");
+const tempoObjetivo4 = new Date("2024-06-01T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
